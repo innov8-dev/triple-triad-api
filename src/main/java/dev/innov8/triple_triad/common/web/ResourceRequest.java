94ba1293f5f287a12b3391c2d2932851a5cf89f1
Wezley Singleton <wezley.singleton@gmail.com>
2021-11-29 00:25:17 -0500

    creature module: renamed NewCreatureRequest -> UpsertCreatureRequest (since UpdateCreatureRequest would have been identical)

--- src/main/java/dev/innov8/triple_triad/common/web/ResourceRequest.java
+++ src/main/java/dev/innov8/triple_triad/common/web/ResourceRequest.java
@@ -3,12 +3,12 @@ package dev.innov8.triple_triad.common.web;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import dev.innov8.triple_triad.creature.dtos.requests.NewCreatureRequest;
+import dev.innov8.triple_triad.creature.dtos.requests.UpsertCreatureRequest;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
 @JsonSubTypes({
-    @JsonSubTypes.Type(NewCreatureRequest.class)
+    @JsonSubTypes.Type(UpsertCreatureRequest.class)
 })
 public interface ResourceRequest<T> {
     T extract();
